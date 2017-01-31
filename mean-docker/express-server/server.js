32395880f123d0529d33cf3bf0609bdb6f955db7
Mariana Valdivia <mariana.valdivia.carbonell@gmail.com>
2017-01-31 11:06:28 -0600

    crud y docker-composer

--- mean-docker/express-server/server.js
+++ mean-docker/express-server/server.js
@@ -31,3 +31,16 @@ const server = http.createServer(app);
  * Listen on provided port, on all network interfaces.
  */
 server.listen(port, () => console.log(`API running on localhost:${port}`));
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
+app.use('/', api);
+
